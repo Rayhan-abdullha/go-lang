cf75ee908b90c9ced44aa49c9189fcba2d0b4ae0
Rayhan-abdullha <programmer.rayhan1@gmail.com>
2024-12-03 10:10:59 +0600

    first commit

---
+++ loop.go
@@ -0,0 +1,44 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// var a = []any{1, 2, 3, 4, 5, 6, 7, "bool", 9, 10}
+
+// func main() {
+// 	fmt.Println("Hello, World!")
+	// var a string = "rayhan"
+	// b := "nazim"
+	// Example("Hello", a)
+	// Example("Hello", b)
+
+	// var a = []any{1, 2, 3, 4, 5, 6, 7, "bool", 9, 10}
+	// Example(a)
+
+	// for i := 0; i < len(a); i++ {
+	// 	fmt.Println(a[i])
+	// }
+	
+	// i := 3
+	// for i >= 1 {
+	// 	Example(a)
+	// 	fmt.Println(i)
+	// 	i--
+	// }
+
+	// for i := range 3 {
+	// 	fmt.Println(i)
+	// }
+
+	// for v, _ := range a {
+	// 	fmt.Println(v)
+	// }
+}
+
+// func Example(a []any) {
+// 	for i, v := range a {
+// 		fmt.Println(i,"=:", v)
+// 	}
+// }
+
