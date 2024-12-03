2aaea2a69c5305b5387363a661bdcd61938dfd3c
Rayhan-abdullha <programmer.rayhan1@gmail.com>
2024-12-03 10:58:01 +0600

    add array

---
+++ array.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// func main() {
+	// var a [5]int
+	// a[4] = 100
+    // fmt.Println("set:", a)
+    // fmt.Println("get:", a[4])
+
+	// b := [...]int{100, 3: 400, 500}
+    // fmt.Println("idx:", b)
+
+	// var twoD [2][3]int
+	// twoD[0][0] = 1
+	// twoD[0][1] = 2
+	// twoD[0][2] = 3
+	// twoD[1][0] = 4
+	// twoD[1][1] = 5
+	// twoD[1][2] = 6
+	// for _, v := range twoD {
+	// 	for _, v2 := range v {
+	// 		fmt.Print(v2, " ")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// var s []any	
+	// s = append(s, 1)
+	// s = append(s, 1)
+	// fmt.Println(s)
+	// c := make([]any, len(s))
+	// copy(c, s)
+	// fmt.Println("cpy:", c)
+// }
---
+++ project.go
@@ -0,0 +1,66 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
+
+// var todoList []string
+
+// func main() {
+// 	scanner := bufio.NewScanner(os.Stdin)
+// 	for {
+// 		fmt.Println("To-Do List CLI")
+// 		fmt.Println("1. Add Task")
+// 		fmt.Println("2. List Tasks")
+// 		fmt.Println("3. Remove Task")
+// 		fmt.Println("4. Exit")
+// 		fmt.Print("Enter your choice: ")
+
+// 		scanner.Scan()
+// 		choice := scanner.Text()
+
+// 		switch choice {
+// 		case "1":
+// 			addTask(scanner)
+// 		case "2":
+// 			listTasks()
+// 		case "3":
+// 			removeTask(scanner)
+// 		case "4":
+// 			fmt.Println("Exiting...")
+// 			return
+// 		default:
+// 			fmt.Println("Invalid choice, please try again.")
+// 		}
+// 	}
+// }
+
+// func addTask(scanner *bufio.Scanner) {
+// 	fmt.Print("Enter task: ")
+// 	scanner.Scan()
+// 	task := scanner.Text()
+// 	todoList = append(todoList, task)
+// 	fmt.Println("Task added.")
+// }
+
+// func listTasks() {
+// 	fmt.Println("To-Do List:")
+// 	for i, task := range todoList {
+// 		fmt.Printf("%d. %s\n", i+1, task)
+// 	}
+// }
+
+// func removeTask(scanner *bufio.Scanner) {
+// 	fmt.Print("Enter task number to remove: ")
+// 	scanner.Scan()
+// 	var taskNumber int
+// 	fmt.Sscanf(scanner.Text(), "%d", &taskNumber)
+// 	if taskNumber > 0 && taskNumber <= len(todoList) {
+// 		todoList = append(todoList[:taskNumber-1], todoList[taskNumber:]...)
+// 		fmt.Println("Task removed.")
+// 	} else {
+// 		fmt.Println("Invalid task number.")
+// 	}
+// }
